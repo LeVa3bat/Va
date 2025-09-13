1969453d1256c19ae54c101a053a1110eed9b907
LeVa3bat <alex.666.881@gmail.com>
2025-09-13 19:19:46 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,8 @@ const CACHE_NAME = 'ammo-accounting-v1';
 const urlsToCache = [
   './',
   './index.html',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
 ];
 
 self.addEventListener('install', event => {
